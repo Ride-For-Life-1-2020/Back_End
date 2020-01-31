cff804cc920da589fdf18c3764147b6f62b0d869
Danny Town <47186976+dannyotown@users.noreply.github.com>
2020-01-30 20:09:55 -0600

    Added Cors And dotenv

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const helmet = require("helmet");
 const server = express();
+const cors = require("cors");
 const patientRoute = require("./routes/patientRoute/patientRoute");
 const driverRoute = require("./routes/driverRoute/driverRoute");
+const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
+server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
 server.use("/api/patient", patientRoute);
 server.use("/api/driver", driverRoute);
+
 server.use((err, req, res, next) => {
   console.log("Error:", err);
   res.status(500).json({
@@ -18,7 +22,7 @@ server.use((err, req, res, next) => {
 });
 
 if (!module.parent) {
-  server.listen(port, () => {
+  server.listen(port, host, () => {
     console.log(`\n=> Server up at http://localhost:${port}\n`);
   });
 }
