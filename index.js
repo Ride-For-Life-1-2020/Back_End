304c0d011418e3141e4f4217886713914d29497c
Danny Town <47186976+dannyotown@users.noreply.github.com>
2020-01-30 23:43:40 -0600

    testing simple get

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 require("dotenv").config();
-const port = process.env.PORT || 80;
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8080;
 const patientRoute = require("./routes/patientRoute/patientRoute");
 const driverRoute = require("./routes/driverRoute/driverRoute");
 
-server.use(helmet());
+// server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/patient", patientRoute);
-server.use("/api/driver", driverRoute);
+// server.use("/api/patient", patientRoute);
+// server.use("/api/driver", driverRoute);
 
 // server.use((err, req, res, next) => {
 //   console.log("Error:", err);
@@ -24,6 +25,6 @@ server.get("/", (req, res) => {
   res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
 });
 
-server.listen(port, () => {
+server.listen(port, host, () => {
   console.log(`\n=> Server up at http://localhost:${port}\n`);
 });
