4301f31dbe07a63f8fd766f986a6e91eb6a10d1f
Dtown <dannyotown@gmail.com>
2020-02-03 20:49:11 -0600

    test commit

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: "pg",
     useNullAsDefault: true,
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    connection: `${process.env.TEST_DB}?ssl=true`,
     migrations: {
       directory: "./data/migrations"
     },
